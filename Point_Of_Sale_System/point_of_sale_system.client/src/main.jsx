ebf060f98bdfef202e3d619d3a59f8d6e64faa9d
Poktamor <ruslanui.m@gmail.com>
2025-11-11 16:14:45 +0200

    New navbar and routing added

--- Point_Of_Sale_System/point_of_sale_system.client/src/main.jsx
+++ Point_Of_Sale_System/point_of_sale_system.client/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import BasicNavbar from './BasicNavbar.jsx';
+import App from './App.jsx';
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/*",
+        element: <h1>Error 404</h1>,
+    }
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <div className="bottomElement ">
+            <BasicNavbar/>
+            <RouterProvider router={router} />
+        </div>
+    </StrictMode>
+);
