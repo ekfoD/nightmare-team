0f0596db42e799744a774e661dffd5b40c7de165
Vanielius1013 <danieliusvickacka@gmail.com>
2025-11-28 20:02:49 +0200

    Added alt layout for appWorker. No way to access it yet

--- Point_Of_Sale_System/point_of_sale_system.client/src/layouts/MainLayout.jsx
+++ Point_Of_Sale_System/point_of_sale_system.client/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../pages/Navbar";
+import Navbar from "../pages/navbars/Navbar";
 
 export default function MainLayout({ children }) {
     return (
