3d982e07ea1641f6ffae8c7e0e1d930f046e3fea
Dovydas Girskas <dovydas.girskas@gmail.com>
2025-12-04 13:09:08 +0200

    Renamed some folders, moved files, deleted not needed .MD files

--- Point_Of_Sale_System/point_of_sale_system.client/src/App.jsx
+++ Point_Of_Sale_System/point_of_sale_system.client/src/App.jsx
@@ -18,9 +18,8 @@ import Employees from './pages/employees/employees.jsx';
 import MenuManagement from './pages/Menu/MenuManagement.jsx';
 
 import AppAbout from './pages/about/AppAbout.jsx';
-import AppArchive from './pages/archives/AppArchive.jsx';
-import Calendar from './pages/calendar/Calendar.jsx';
 import Schedule from './pages/schedule/Schedule.jsx';
+import OrderHistory from './pages/history/OrderHistory.jsx';
 
 const BASE_URL = "http://localhost:5098"
 
@@ -43,8 +42,8 @@ function App() {
                     </Route>
                     <Route element={<AppLayout />}>
                         <Route path="/appAbout" element={<AppAbout />} /> 
-                        <Route path="/appArchive" element={<AppArchive />} />
-                        <Route path="/calendar" element={<Calendar />} />
+                        <Route path="/history" element={<OrderHistory /> }/>
+                        {/* <Route path="/calendar" element={<Calendar />} /> */}
                         <Route path="/schedule" element={<Schedule />} />
                     </Route>
                 </Route>
