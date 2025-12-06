47ee8e0f3eabcb46a6029de7666a3cb2e87f9d00
solidHebi <pnavikas87@gmail.com>
2025-12-06 12:44:01 +0200

    working form for organization editing

--- Point_Of_Sale_System/point_of_sale_system.client/src/pages/business/EditBusiness.jsx
+++ Point_Of_Sale_System/point_of_sale_system.client/src/pages/business/EditBusiness.jsx
@@ -1,18 +1,23 @@
 import EditForm from "./EditForm"
 
 function EditBusiness() {
-    // const bussines = {
-    //     name: "VACKACKA INDUSTRY",
-    //     address: "Lithuania Kaunas Vilniaus g. 15",
-    //     emailAddress: "danieliukas67@gmail.com",
-    //     phoneNumber: "+370676767",
-    //     bussinesType: "order-service",
-    //     currency: "$",
-    // }
+    const bussines = {
+        name: "VACKACKA INDUSTRY",
+        address: "Lithuania Kaunas Vilniaus g. 15",
+        emailAddress: "danieliukas67@gmail.com",
+        phoneNumber: "+37012345678",
+        bussinesType: "service",
+        currency: "€",
+    }
+
+    function handleFormSubmit(updatedData){
+        console.log("Updated business:", updatedBusiness);
+        // send to backend
+    }
     return (
         <>
             <h1>Edit business</h1>
-            <EditForm></EditForm>
+            <EditForm onSubmit={handleFormSubmit} business={bussines} />
         </>
     );
 }
--- Point_Of_Sale_System/point_of_sale_system.client/src/pages/business/EditForm.jsx
+++ Point_Of_Sale_System/point_of_sale_system.client/src/pages/business/EditForm.jsx
@@ -3,62 +3,106 @@ import { Form, Button, Row, Col, Card } from "react-bootstrap";
 
 // export default function EditForm({ business, OnSave, OnCancel }) {
 
-export default function EditForm({ }) {
-    // const [formData, setFormData] = useState({
-    //     name: business.name || "",
-    //     address: business.address || "",
-    //     emailAddress: business.emailAddress || "",
-    //     phoneNumber: business.phoneNumber || "",
-    //     bussinesType: business.bussinesType || "",
-    //     currency: business.currency || "",
-    // });
+export default function EditForm({ OnSubmit, business }) {
+    const [formData, setFormData] = useState({
+        name: business.name || "",
+        address: business.address || "",
+        emailAddress: business.emailAddress || "",
+        phoneNumber: business.phoneNumber || "",
+        bussinesType: business.bussinesType || "",
+        currency: business.currency || "",
+    });
 
-    // const handleChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setFormData(prev => ({ ...prev, [name]: value }));
-    // };
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-    //     OnSave(formData);   // send data back to parent
-    // };
+    function handleCancel(e) {
+        e.preventDefault();
+        setFormData({
+            name: business.name || "",
+            address: business.address || "",
+            emailAddress: business.emailAddress || "",
+            phoneNumber: business.phoneNumber || "",
+            bussinesType: business.bussinesType || "",
+            currency: business.currency || "",
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form submitted:", formData);
+        OnSubmit(formData);   // send data back to parent
+    };
+
     return (
         <Card className="p-4 shadow-sm">
             <h3 className="mb-3">Organization settings</h3>
             <Form>
                 <Form.Group className="mb-3" controlId="formName">
                     <Form.Label>Organization name</Form.Label>
-                    <Form.Control type="string" />
+                    <Form.Control
+                        type="string"
+                        value={formData.name}
+                        name="name"
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formAddress">
                     <Form.Label>Organization address</Form.Label>
-                    <Form.Control type="string" />
-
+                    <Form.Control
+                        type="string"
+                        value={formData.address}
+                        name="address"
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formPhone">
                     <Form.Label>Organization phone number</Form.Label>
-                    <Form.Control type="phone" pattern="[0-9]{3}" />
+                    <Form.Control
+                        type="phone"
+                        pattern="\+\d{11}"
+                        value={formData.phoneNumber}
+                        name="phoneNumber"
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formOrganizationType">
-                    <Form.Label>Choose </Form.Label>
-                    <Form.Select >
-                        <option>Select any value</option>
-                        <option>Order</option>
-                        <option>Service</option>
+                    <Form.Label>Choose organization type</Form.Label>
+                    <Form.Select
+                        value={formData.type}
+                        name="type"
+                        onChange={handleChange}
+                    >
+                        <option value="order">Order</option>
+                        <option value="service">Service</option>
                     </Form.Select>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formCurrency">
                     <Form.Label>Choose currency</Form.Label>
-                    <Form.Select>
-                        <option>Select any value</option>
-                        <option>$</option>
-                        <option>€</option></Form.Select>
+                    <Form.Select
+                        value={formData.currency}
+                        name="currency"
+                        onChange={handleChange}
+                    >
+                        <option value="$">USD - $</option>
+                        <option value="€">EUR - €</option>
+                    </Form.Select>
                 </Form.Group>
 
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Save Changes
+                </Button>
+                <Button variant="primary" type="cancel" onClick={handleCancel}>
+                    Cancel
+                </Button>
             </Form>
         </Card>
     );
