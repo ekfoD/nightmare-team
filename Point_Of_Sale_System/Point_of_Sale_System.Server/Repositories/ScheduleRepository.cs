fb6a12d6d9345ef2249aa22458d1915be6fa55a7
solidHebi <pnavikas87@gmail.com>
2025-12-06 20:36:40 +0200

    Commented file with errors

--- Point_Of_Sale_System/Point_of_Sale_System.Server/Repositories/ScheduleRepository.cs
+++ Point_Of_Sale_System/Point_of_Sale_System.Server/Repositories/ScheduleRepository.cs
@@ -1,35 +1,35 @@
-using System.Text.Json;
-using Point_of_Sale_System.Server.Models;
+// using System.Text.Json;
+// using Point_of_Sale_System.Server.Models;
 
-public class MockScheduleRepository
-{
-    private readonly List<Schedule> _schedules;
+// public class MockScheduleRepository
+// {
+//     private readonly List<Schedule> _schedules;
 
-    public MockScheduleRepository()
-    {
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true,
-            Converters =
-            {
-                new DateOnlyJsonConverter(),
-                new TimeOnlyJsonConverter()
-            }
-        };
+//     public MockScheduleRepository()
+//     {
+//         var options = new JsonSerializerOptions
+//         {
+//             PropertyNameCaseInsensitive = true,
+//             Converters =
+//             {
+//                 new DateOnlyJsonConverter(),
+//                 new TimeOnlyJsonConverter()
+//             }
+//         };
 
-        var json = File.ReadAllText("Data/schedules.json");
-        _schedules = JsonSerializer.Deserialize<List<Schedule>>(json, options) ?? new List<Schedule>();
-    }
+//         var json = File.ReadAllText("Data/schedules.json");
+//         _schedules = JsonSerializer.Deserialize<List<Schedule>>(json, options) ?? new List<Schedule>();
+//     }
 
-    public Task<IEnumerable<Schedule>> GetSchedulesByDateAsync(DateOnly date)
-    {
-        var result = _schedules.Where(s => s.Date == date);
-        return Task.FromResult(result);
-    }
+//     public Task<IEnumerable<Schedule>> GetSchedulesByDateAsync(DateOnly date)
+//     {
+//         var result = _schedules.Where(s => s.Date == date);
+//         return Task.FromResult(result);
+//     }
 
-    public Task<IEnumerable<DateOnly>> GetSummaryAsync()
-    {
-        var dates = _schedules.Select(s => s.Date).Distinct();
-        return Task.FromResult(dates);
-    }
-}
+//     public Task<IEnumerable<DateOnly>> GetSummaryAsync()
+//     {
+//         var dates = _schedules.Select(s => s.Date).Distinct();
+//         return Task.FromResult(dates);
+//     }
+// }
