8542c772ec9d85bccee2941c2ecd4e65857d14f5
Vanielius1013 <danieliusvickacka@gmail.com>
2025-12-16 10:23:20 +0200

    Changed DB discount and gruoped up DTOs for cleanliness

--- Point_Of_Sale_System/Point_of_Sale_System.Server/DTOs/MenuServiceDTOs.cs
+++ Point_Of_Sale_System/Point_of_Sale_System.Server/DTOs/MenuServiceDTOs.cs
@@ -1,7 +1,20 @@
+using System.Runtime.Versioning;
 using Point_of_Sale_System.Server.Enums;
 
 namespace Point_of_Sale_System.Server.DTOs
 {
+    public class MenuServiceDto
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required int Duration { get; set; } // HH:MM format
+        public decimal Price { get; set; }
+        public required string Description { get; set; }
+        public StatusEnum Status { get; set; }
+        public CurrencyEnum Currency { get; set; }
+        public List<TaxDTO> Taxes { get; set; } = new();
+    }
+
     public class CreateMenuServiceDto
     {
         public required string Name { get; set; }
--- Point_Of_Sale_System/Point_of_Sale_System.Server/Models/Entities/OrdersAndPayments/Discount.cs
+++ Point_Of_Sale_System/Point_of_Sale_System.Server/Models/Entities/OrdersAndPayments/Discount.cs
@@ -1,7 +1,9 @@
 ﻿using Point_of_Sale_System.Server.Enums;
+using Point_of_Sale_System.Server.Models.Entities.Business;
 using Point_of_Sale_System.Server.Models.Entities.MenuBased;
 using Point_of_Sale_System.Server.Models.Entities.ServiceBased;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Point_of_Sale_System.Server.Models.Entities.OrdersAndPayments
 {
@@ -10,15 +12,19 @@ namespace Point_of_Sale_System.Server.Models.Entities.OrdersAndPayments
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
         public required decimal Amount { get; set; }
-        public NumberTypeEnum NumberType { get; set; }
         public AppliedToEnum ApplicableTo { get; set; }
         public DateOnly ValidFrom { get; set; }
         public DateOnly ValidUntil { get; set; }
         public StatusEnum Status { get; set; }
         public DateTime Timestamp { get; set; } = DateTime.Now;
-
+        
         //navigation to FK's
-        public virtual ICollection<MenuItem> MenuItems { get; set; }
-        public virtual ICollection<MenuService> MenuServices { get; set; }
+        public required Guid OrganizationId { get; set; }
+
+        [ForeignKey("OrganizationId")]
+        public required Organization Organization { get; set;}
+        public virtual ICollection<Order>? Orders { get; set; }
+        public virtual ICollection<MenuItem>? MenuItems { get; set; }
+        public virtual ICollection<MenuService>? MenuServices { get; set; }
     }
 }
